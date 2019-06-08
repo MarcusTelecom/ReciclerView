c05791addfdd8fdebdb80ceef41db71d9d5d5ceb
MarcusTelecom <marcus.ufsj@gmail.com>
2019-06-08 01:15:50 -0300

    Criando o item e o ViewHolder

---
+++ app/src/main/java/telecom/marcus/reciclerview/ViewHolder.java
@@ -0,0 +1,24 @@
+package telecom.marcus.reciclerview;
+
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    public ImageView featuredImage;
+    public TextView title;
+    public TextView desc;
+    public AppCompatButton btnLink;
+
+    public ViewHolder(View itemView) {
+        super(itemView);
+
+        this.featuredImage = itemView.findViewById(R.id.featuredImage);
+        this.title = itemView.findViewById(R.id.title);
+        this.desc = itemView.findViewById(R.id.desc);
+        this.btnLink = itemView.findViewById(R.id.btnLink);
+    }
+}
